0add05aac071d2c7929e6a2243ce85636c03f380
asansantiago316 <asan.santiagoet36@gmail.com>
2025-08-11 18:31:07 -0300

    Add files via upload

--- escaner_de_red/src/escaner_de_red/pantalla_principal.java
+++ escaner_de_red/src/escaner_de_red/pantalla_principal.java
@@ -3,7 +3,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.lang.*;
+import java.util.*;
+import java.io.*;
 
 
 public class pantalla_principal extends JFrame implements ActionListener{
@@ -16,6 +19,7 @@ public class pantalla_principal extends JFrame implements ActionListener{
 	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	     frame.setSize(400, 200);
 	     frame.setLocationRelativeTo(null);
+	  
 		
 		JPanel panel1 = new JPanel(new GridLayout (3, 4, 10, 10));
 		panel1.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
@@ -23,9 +27,6 @@ public class pantalla_principal extends JFrame implements ActionListener{
 		panel1.add(new JLabel("IP"));
 		txtIP = new JTextField();
 		panel1.add(txtIP);
-
-		
-	
 		
 		btnescaner = new JButton("Ejecutar Escaneo");
 		panel1.add(btnescaner);
@@ -34,8 +35,6 @@ public class pantalla_principal extends JFrame implements ActionListener{
 		
 		btnescaner.addActionListener(this);
 		btnlimpiar.addActionListener(this);
-		
-		
 		
 		frame.setContentPane(panel1);
 	    frame.setVisible(true);
@@ -47,10 +46,22 @@ public class pantalla_principal extends JFrame implements ActionListener{
 	  public void actionPerformed(ActionEvent e) {
 		  
 			if (e.getSource() == btnescaner) {
-				JOptionPane.showInputDialog(null, "hola");
+				String obtener_ip = txtIP.getText();
+				Scanner sc = new Scanner (obtener_ip);
+				int ip_a_int = Integer.parseInt(obtener_ip); 
+				try {
+					Process buscarIP = Runtime.getRuntime().exec("cmd /c ping " + ip_a_int); 
+					BufferedReader lector = new BufferedReader(new InputStreamReader(buscarIP.getInputStream()));
+				}
+				catch (IOException e1) {
+					JOptionPane.showMessageDialog(null, "no existe la ip");
+				}
+				JOptionPane.showInputDialog(null, "elje el rango de numeros a escanear(max 255)");
 			}
 	  }
-	
+	  
+	  	
+
 	public static void main(String[] args) {
 		pantalla_principal gui = new pantalla_principal();
 	
